e66b22f93764061e7976858df21a78dcd163c275
YAMAKI YUKIYA <96877368+yukiya1006@users.noreply.github.com>
2022-05-29 09:48:23 +0900

    firstcommit

---
+++ src/pases/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Topbar from '../../compornents/topbar/Topbar';
+
+export default function Home() {
+  return (
+    <>
+    <Topbar />
+    {/* <Sidebar />
+    <Timeline />
+    <Rightbar /> */}
+    </>
+  );
+}
+
